cf309522b7850fd6d889dfd05833a3fb46d202c7
Olesia2805 <vysotina2002@gmail.com>
2024-10-06 10:14:28 +0200

    add Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,33 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { getCountries } from '../service/countryApi';
+import { useEffect, useState } from 'react';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await getCountries();
+        setCountries(response);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+        {loading && <Loader />}
+        <CountryList countries={countries} />
+        {error && <Heading title={error} bottom />}
       </Container>
     </Section>
   );
